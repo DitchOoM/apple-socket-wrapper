225944e16c005beb3f27834be346a8a18f4fc148
Rahul Behera <rbehera@gmail.com>
2023-01-18 17:25:45 -0800

    Initial commit

---
+++ Example/SocketWrapper/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SocketWrapper
+//
+//  Created by Rahul Behera on 01/18/2023.
+//  Copyright (c) 2023 Rahul Behera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
